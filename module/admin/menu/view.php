5e70bc8fc0f85f47f94ebd7b50b9f1da8e14cd94
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-05 18:50:45 +0100

    COMMIT 5315-48

--- module/admin/menu/view.php
+++ module/admin/menu/view.php
@@ -216,6 +216,13 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                                                                 <span class="help-block">This is a help text</span>
                                                             </div>
                                                         </div>
+                                                        <div class="form-group">
+                                                            <label class="col-md-3 control-label" for="example-text-input">Text Input</label>
+                                                            <div class="col-md-9">
+                                                                <input type="text" id="example-text-input" name="example-text-input" class="form-control" placeholder="Text">
+                                                                <span class="help-block">This is a help text</span>
+                                                            </div>
+                                                        </div>
 
 
                                                     </form>
