ff3f64ae442f72c030ae9eea33168607b11fc78f
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-05 16:52:18 +0100

    COMMIT 5315-24

--- inc/db.conf.php
+++ inc/db.conf.php
@@ -4,7 +4,7 @@ function BaseConnect(){
 	define("HOST", "localhost");
 	define("USER", "remote-user");
 	define("PASS", "1992maxime");
-	define("BASE", "ristogest");
+	define("BASE", "ristoria");
 
 	$sql_connect = mysql_connect(HOST, USER, PASS)or die(mysql_error());
 	$sql_select_db = mysql_select_db(BASE)or die(mysql_error());
