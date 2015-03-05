45c9566caa0df93dfa54fa38abfc7ce924b11558
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-05 18:36:29 +0100

    COMMIT 5315-42

--- module/admin/menu/view.php
+++ module/admin/menu/view.php
@@ -203,47 +203,14 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                                    <h3 class="modal-title">Modification d'un article</h3>
+                                                    <h3 class="modal-title">Modal Title</h3>
                                                 </div>
                                                 <div class="modal-body">
-                                                    <form class="form-bordered form-horizontal" action="<?php echo SITE,FOLDER; ?>inc/control/menu.php" method="POST">
-                                                        <input type="hidden" name="idarticlemenu" value="<?php echo $donnee_article['idarticlemenu']; ?>" />
-
-                                                        <div class="form-group">
-                                                            <label class="col-md-3 control-label" for="example-text-input">Designation de l'article</label>
-                                                            <div class="col-md-9">
-                                                                <input type="text" id="example-text-input" name="designation" class="form-control" value="<?php echo $donnee_article['designation']; ?>">
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group">
-                                                            <label class="col-md-3 control-label" for="example-textarea-input">Description de l'article</label>
-                                                            <div class="col-md-9">
-                                                                <textarea id="example-textarea-input" name="description" rows="9" class="form-control"><?php echo $donnee_article['description']; ?></textarea>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group">
-                                                            <label class="col-md-4 control-label" for="val_number">Prix Unitaire</label>
-                                                            <div class="col-md-6">
-                                                                <div class="input-group">
-                                                                    <input type="text" id="val_number" name="prix_unitaire" class="form-control" value="<?php echo $donnee_article['prix_unitaire']; ?>">
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="form-group form-actions">
-                                                            <div class="pull-left">
-                                                                <button type="submit" class="btn btn-success" name="add-article-control" value="Valider"><i class="fa fa-check-circle-o"></i> Valider</button>
-                                                            </div>
-                                                            <div class="pull-right">
-                                                                <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times-circle-o"></i> Fermer la boite de dialogue</button>
-                                                            </div>
-                                                        </div>
-
-
-
-                                                    </form>
+                                                    Modal Content..
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-sm btn-primary">Save changes</button>
                                                 </div>
                                             </div>
                                         </div>
