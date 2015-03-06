28e4111d5ec8f5a91f2fcf604090d131ab907073
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-06 11:43:26 +0100

    COMMIT 6315-8

--- inc/control/commande.php
+++ inc/control/commande.php
@@ -8,10 +8,10 @@ if(isset($_POST['add-commande-control']) && $_POST['add-commande-control'] == 'V
 {
     $idmenu = $_POST['idmenu'];
     $iduser = $_POST['iduser'];
-    $num_commande = "ORD.UTS".rand(100, 99999);
+    $num_commande = "ORD.UTS.".rand(100, 99999);
 
-    $sql_add_commande = mysql_query("INSERT INTO `commande`(`idcommande`, `num_commande`, `date_commande`, `iduser`, `montant_total`)
-                        VALUES (NULL,'$num_commande','','$iduser','0')")or die(mysql_error());
+    $sql_add_commande = mysql_query("INSERT INTO `commande`(`idcommande`, `idmenu`, `num_commande`, `date_commande`, `iduser`, `montant_total`)
+VALUES (NULL,'$idmenu','$num_commande','','$iduser','0')")or die(mysql_error());
 
     //Import commande pour redirection
     $sql_import_commande = mysql_query("SELECT * FROM commande WHERE num_commande = '$num_commande'")or die(mysql_error());
