002ea2321af966e7ebf1eb024e6a087ce910a3f5
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-06 11:45:32 +0100

    COMMIT 6315-9

--- module/commande/index.php
+++ module/commande/index.php
@@ -159,7 +159,8 @@ $li_end = "<li><a href='#'>".TITLE_PAGE."</a></li>";
                                 </thead>
                                 <tbody>
                                 <?php
-                                $sql_commande = mysql_query("SELECT * FROM commande, menu WHERE commande.idmenu = menu.idmenu")or die(mysql_error());
+                                $sql_commande = mysql_query("SELECT * FROM commande, menu, utilisateur WHERE commande.idmenu = menu.idmenu
+                                                AND commande.iduser = utilisateur.iduser")or die(mysql_error());
                                 while($donnee_commande = mysql_fetch_array($sql_commande)) {
                                     ?>
                                     <tr>
