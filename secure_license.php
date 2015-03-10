4a8ab2ebe61776f6e14e114271a52ea185b812c4
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-10 10:23:16 +0100

    COMMIT 10315

---
+++ secure_license.php
@@ -0,0 +1,10 @@
+<?php
+//HASH SECURE
+define("SECURE_HASH", ""); // Verification du Hash tag du programme
+define("VERSION_KERNEL", ""); //Verification de la verdion du programme
+define("UNIQUE_LICENSER", ""); // Clé d'identification Utilisateur & Société
+define("KEY", ""); //License d'utilisation
+define("DOMAIN", ""); //Nom de domaine utiliser et status
+define("IP", ""); //IP du serveur de connection
+define("DIRECTORY", ""); //Link secure_license.php
+
