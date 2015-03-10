967bb4ac00c25575c7025f4ff7d22f0611c5addd
CRIDIP-SWD <mmockelyn@cridip.com>
2015-03-10 11:12:08 +0100

    COMMIT 10315-8

--- assets/js/datepicker-fr.js
+++ assets/js/datepicker-fr.js
@@ -1,25 +1,27 @@
 /* French initialisation for the jQuery UI date picker plugin. */
 /* Written by Keith Wood (kbwood{at}iinet.com.au),
- Stéphane Nahmani (sholby@sholby.net),
- Stéphane Raimbault <stephane.raimbault@gmail.com> */
+ StÃ©phane Nahmani (sholby@sholby.net),
+ StÃ©phane Raimbault <stephane.raimbault@gmail.com> */
 (function( factory ) {
     if ( typeof define === "function" && define.amd ) {
-// AMD. Register as an anonymous module.
-        define([ "app.js" ], factory );
+
+        // AMD. Register as an anonymous module.
+        define([ "vendor/jquery-1.11.1.min.js" ], factory );
     } else {
-// Browser globals
+
+        // Browser globals
         factory( jQuery.datepicker );
     }
 }(function( datepicker ) {
     datepicker.regional['fr'] = {
         closeText: 'Fermer',
-        prevText: 'Précédent',
+        prevText: 'PrÃ©cÃ©dent',
         nextText: 'Suivant',
         currentText: 'Aujourd\'hui',
-        monthNames: ['janvier', 'février', 'mars', 'avril', 'mai', 'juin',
-            'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
-        monthNamesShort: ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin',
-            'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'],
+        monthNames: ['janvier', 'fÃ©vrier', 'mars', 'avril', 'mai', 'juin',
+            'juillet', 'aoÃ»t', 'septembre', 'octobre', 'novembre', 'dÃ©cembre'],
+        monthNamesShort: ['janv.', 'fÃ©vr.', 'mars', 'avril', 'mai', 'juin',
+            'juil.', 'aoÃ»t', 'sept.', 'oct.', 'nov.', 'dÃ©c.'],
         dayNames: ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'],
         dayNamesShort: ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.'],
         dayNamesMin: ['D','L','M','M','J','V','S'],
@@ -30,5 +32,7 @@
         showMonthAfterYear: false,
         yearSuffix: ''};
     datepicker.setDefaults(datepicker.regional['fr']);
+
     return datepicker.regional['fr'];
+
 }));
