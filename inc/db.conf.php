6973570c70666c26813395a961662208d15c8fff
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-27 10:03:39 +0100

    RSEY

--- inc/db.conf.php
+++ inc/db.conf.php
@@ -1,8 +1,8 @@
 <?php 
 	$host = "localhost";
-	$user = "root";
-	$pass = "1992maxime";
-	$base = "ristoria";
+	$user = "slts_resto";
+	$pass = "azerty1";
+	$base = "slts_resto";
 
 	$sql_connect = mysql_connect($host, $user, $pass)or die(mysql_error());
 	$sql_select_db = mysql_select_db ($base) or die('Error ' . $base . ' : ' . mysql_error());
