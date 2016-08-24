d60c7839a7b56d77eb32bae56a766a61d0c60a0d
CRIDIP-SWD <developpement@cridip.com>
2016-08-24 18:27:06 +0200

    CORRECTOR

--- inc/footer.php
+++ inc/footer.php
@@ -6,3 +6,4 @@
                         <span id="year-copy"></span> &copy; <a href="http://www.restogest.com" target="_blank">Restogest</a>
                     </div>
                 </footer>
+                <script type="text/javascript" src="//chat.restogest.com/php/app.php?widget-init.js"></script>
