a80934cde7819e887706c65fbcc5d1ddb9386d57
mmockelyn <sysdev@cridip.com>
2016-09-08 14:51:16 +0200

    V1.1.1

--- cron/task.php
+++ cron/task.php
@@ -9,11 +9,12 @@ require_once "../inc/db.conf.php";
 $semaine = date("W");
 
 $sql_menu = mysql_query("SELECT * FROM menu WHERE semaine = '$semaine'")or die(mysql_error());
-$menu = mysql_fetch_array($sql_menu);
-$idmenu = $menu['idmenu'];
 
-$sql_commande = mysql_query("SELECT * FROM commande WHERE idmenu = '$idmenu' AND etat_commande = 0")or die(mysql_error());
-while ($commande = mysql_fetch_array($sql_commande)){
-    $sql_update = mysql_query("UPDATE commande SET etat_commande = 1 WHERE idmenu = '$idmenu'")or die(mysql_error());
+while($menu = mysql_fetch_array($sql_menu)){
+    $idmenu = $menu['idmenu'];
+    $sql_commande = mysql_query("SELECT * FROM commande WHERE idmenu = '$idmenu' AND etat_commande = 0")or die(mysql_error());
+    while ($commande = mysql_fetch_array($sql_commande)){
+        $sql_update = mysql_query("UPDATE commande SET etat_commande = 1 WHERE idmenu = '$idmenu'")or die(mysql_error());
+    }
 }
 
