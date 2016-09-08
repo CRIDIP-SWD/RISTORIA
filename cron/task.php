b397640ae137255ce3dacde18530ee0bd419100b
mmockelyn <sysdev@cridip.com>
2016-09-08 14:48:12 +0200

    V1.1.1

--- cron/task.php
+++ cron/task.php
@@ -15,6 +15,6 @@ $idmenu = $menu['idmenu'];
 $sql_commande = mysql_query("SELECT * FROM commande WHERE idmenu = '$idmenu' AND etat_commande = 0")or die(mysql_error());
 while ($commande = mysql_fetch_array($sql_commande)){
     $sql_update = mysql_query("UPDATE commande SET etat_commande = 1 WHERE idcommande = ".$commande['idcommande'])or die(mysql_error());
-    var_dump($commande);
+    var_dump($sql_commande);
 }
 
